17e284d36351cd7ba852dfced1b16b202fb38f1f
Saurabh Mahajan <saurabh7820@gmail.com>
2017-08-05 21:46:54 +0530

    Basic aurelia routing added

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,24 @@
 export class App{
     
+    configureRouter(config, router){
+        this.router = router;
+        config.options.root = '/';
+        config.map([
+            {
+                route: ['', 'home'],
+                moduleId: PLATFORM.moduleName('books/list'),
+                nav: true,
+                name: 'book-list',
+                title: 'Home'
+            },
+            {
+                route: 'account',
+                moduleId: PLATFORM.moduleName('account/account'),
+                nav: true,
+                name: 'account',
+                title: 'Account'
+            }
+        ]);
+    }
+
 }
